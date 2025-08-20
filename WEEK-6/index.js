1ab2240ff142b56db0ba2368f26c84e35171e7eb
petergriffin29 <rishirajjsr20@gmail.com>
2025-08-21 03:41:16 +0530

    jwt

--- WEEK-6/index.js
+++ WEEK-6/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "randomrishiloverishi";
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-} 
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// } 
 app.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -51,7 +53,9 @@ app.post('/signin', (req, res) => {
     });
 
     if (foundUser){
-        const token = generateToken();
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET) ;// converting username to JWT
         foundUser.token = token;
 
         res.json({
@@ -69,17 +73,35 @@ app.post('/signin', (req, res) => {
 });
 app.get("/me", (req, res) => {
     const token = req.headers.token;
-    const user = users.find(user => user.token === token);
-    if (user) {
-        res.send({
-            username: user.username,
-            password: user.password,
-        })
-    } else {
-        res.status(401).send({
-            message: "Unauthorized"
-        })
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
     }
-})
 
-app.listen(3000);
+    let decodedInformation;
+    try {
+        decodedInformation = jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({ message: 'Token invalid' });
+    }
+
+    const username = decodedInformation.username;
+
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token == token) {
+            foundUser = users[i];
+            break;
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        });
+    } else {
+        res.status(404).json({ message: "token invalid" });
+    }
+});
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
